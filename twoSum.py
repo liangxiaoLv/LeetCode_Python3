ae5fc5b56a80437ed52fa908cf6032920b8c0ea3
liangxiaoLv <130643150+liangxiaoLv@users.noreply.github.com>
2023-12-15 14:55:41 +0800

    Update twoSum.py

--- twoSum.py
+++ twoSum.py
@@ -1,3 +1,14 @@
+# Solution 1
+# brute force solution  暴力求解
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for i in range(1,len(nums)):
+            for j in range(0,i):
+                if nums[i]+nums[j]==target:
+                    return [j,i]
+
+# Solution 2 (better)
+# using hashtable 使用哈希表
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         hashtable = dict()
@@ -7,3 +18,4 @@ class Solution:
             else:
                 hashtable[nums[i]]=i
         return []
+
