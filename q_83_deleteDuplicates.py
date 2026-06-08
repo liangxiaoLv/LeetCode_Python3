47da6d75ffb27e717a0aea06a05831d287050986
liangxiaoLv <liangxiao_lv_1022@163.com>
2026-06-08 15:06:40 +0800

    q83

---
+++ q_83_deleteDuplicates.py
@@ -0,0 +1,50 @@
+# 给定一个已排序的链表的头
+#  head ， 删除所有重复的元素，使每个元素只出现一次 。返回 已排序的链表 。
+#
+#
+#
+#  示例 1：
+#
+#
+# 输入：head = [1,1,2]
+# 输出：[1,2]
+#
+#
+#  示例 2：
+#
+#
+# 输入：head = [1,1,2,3,3]
+# 输出：[1,2,3]
+#
+#
+#
+#
+#  提示：
+#
+#
+#  链表中节点数目在范围 [0, 300] 内
+#  -100 <= Node.val <= 100
+#  题目数据保证链表已经按升序 排列
+#
+#
+#  Related Topics 链表 👍 1265 👎 0
+
+
+# leetcode submit region begin(Prohibit modification and deletion)
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if (head == None):
+            return None
+        now_node = head
+        while now_node.next:
+            if (now_node.next.val == now_node.val):
+                now_node.next = now_node.next.next
+            else:
+                now_node = now_node.next
+        return head
+# leetcode submit region end(Prohibit modification and deletion)
